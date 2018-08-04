508102f8ed3fce47ca281d909cb75811fa695560
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-08-04 01:50:02 -0700

    adds more html js and css

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+new Vue({
+  el: '#app',
+  data: {
+    full_name: 'Aaron Villanueva',
+    pic: "https://yt3.ggpht.com/a-/ACSszfE8jr9X-8TOrsyUy2zRAX1F_6ZZz9knfCIv0w=s900-mo-c-c0xffffffff-rj-k-no",
+    Id: Math.floor(1000 + Math.random() * 9000),
+    date: mm + '/' + dd + '/' + yyyy,
+    time: myTimer()
+  }
+})
