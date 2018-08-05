cfa834cb674839ed8fedbf037371a372931de18a
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-08-05 00:08:03 -0700

    adds more js, css, html

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-//like an ajax call getting information from a server
-//but instead of an ajax call build a way to get the values 
-//to get values user inputs
-//place the values in an object
-//use that object as the data portion in vue
-//the trick is to load the page hiding the div app
-//once the user clicks submit hide the form and show the div app
-//feel free to use animations
-
 document.querySelector('#main-form').addEventListener('submit', function(e){
   e.preventDefault();
 });
@@ -16,7 +7,11 @@ document.querySelector('#submit').addEventListener('click', function() {
   new Vue({
     el: '#app',
     data: {
-      fullName: document.querySelector('#f-name').value + ' ' +  document.querySelector('#l-name').value
+      fullName: document.querySelector('#f-name').value + ' ' +  document.querySelector('#l-name').value,
+      gender: 'Male',
+      email: 'Email',
+      phone: 'Phone',
+      color: 'Red'
     }
   });
 
